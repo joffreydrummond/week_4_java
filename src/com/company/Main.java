a6e8a54095fd0a5caae6b3c311277ac3ca8219c6
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-02-14 12:30:31 -0700

    created project + finished #1

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 1.	Create an instance of an ArrayList of String called employeeNames
+        List<String> employeeNames = new ArrayList<>();
+        //2.	Create an instance of a HashSet of Integer called ids
+
+        //3.	Create an instance of a HashMap of Integer, String called employeeMap
+
+    }
+}
