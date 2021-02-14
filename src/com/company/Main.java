a75ae6e58949779e12e5d722478b0836c837f4da
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-02-14 12:39:24 -0700

    Finished 4 - added employee names and ids

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,5 +11,23 @@ public class Main {
         Set<Integer> ids = new HashSet<>();
         //3.	Create an instance of a HashMap of Integer, String called employeeMap
         Map<Integer, String> employeeMap = new HashMap<>();
+
+        //4.	Add at least five entries to the employeeNames and ids
+        // (make sure both collections have the same number of entries).
+        employeeNames.add("Tom Cruise");
+        employeeNames.add("Denzel Washington");
+        employeeNames.add("Tiger Woods");
+        employeeNames.add("Seth Rogen");
+        employeeNames.add("Pete Davidson");
+
+        ids.add(0);
+        ids.add(1);
+        ids.add(2);
+        ids.add(3);
+        ids.add(4);
+
+
+
+
     }
 }
